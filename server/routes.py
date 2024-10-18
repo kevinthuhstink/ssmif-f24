053f2bafd1ef2f057a5a3a1a45864076f5b2a111
Kevin Cheng <kev72eat@gmail.com>
2024-10-18 19:44:05 -0400

    hook up model to the server route

--- server/routes.py
+++ server/routes.py
@@ -1,14 +1,16 @@
 """ Routes for core trader functionality.
-All routes should have a "status" field.
+All route responses should have a "status" field.
 """
 
 from flask import Blueprint, jsonify, request
 from flask_cors import CORS
 from util import require_json_params
+from services.returns import Carhart4FactorModel
 from services.model import Model, TickerException
 
 core_blueprint = Blueprint("core", __name__, url_prefix="/")
 CORS(core_blueprint)
+returns_model = Carhart4FactorModel()
 
 
 @core_blueprint.route("healthcheck")
@@ -19,7 +21,7 @@ def healthcheck():
 
 @core_blueprint.put("model")
 @require_json_params(["value", "tickers"])
-def portfolio():
+def get_weights():
     """ Returns the desired weight of each stock.
 
     :param value: The total value of the portfolio in USD
@@ -29,20 +31,26 @@ def portfolio():
     """
     body = request.get_json()
     value = body["value"]
-    tickers = body["tickers"]
+    tickers = list(map(lambda t: t.upper(), body["tickers"]))
 
     try:
-        model = Model(value, tickers)
+        model = Model(returns_model, tickers)
     except TickerException as e:
         return jsonify({
-            "status": "failure",
+            "status": "ERROR",
             "error": e.message
             }), 400
 
+    print(model) # NO LOGGER 💀
+    portfolio = model.max_sharpe(risk_free_rate=model.risk_free_rate)
+    returns = (model.portfolio_returns(portfolio) - 1) * value
+    volatility = model.portfolio_risk(portfolio)
+    sharp = model.sharpe_ratio(portfolio)
+
     return jsonify({
         "status": "OK",
-        "weights": model(),
-        "return": 0,
-        "volatility": 0,
-        "sharpe": 0
+        "weights": portfolio,
+        "return": returns,
+        "volatility": volatility,
+        "sharpe": sharp
         })
