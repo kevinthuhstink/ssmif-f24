f0b1ba8b9eb01bdfba0759addcdc1f47d3e1fb84
Kevin Cheng <kev72eat@gmail.com>
2024-10-19 23:46:13 -0400

    refactor to include error field

--- client/src/app/context/authContext.tsx
+++ client/src/app/context/authContext.tsx
@@ -1,15 +1,21 @@
 import { createContext, useContext, useState } from "react"
+import { errorSchema } from "../baseRequest"
 import { z } from "zod"
 
-export const jwtSchema = z.string()
+export const jwtSchema = z.object({
+  jwt: z.string().optional(),
+  error: errorSchema.shape.error.optional(),
+})
+export type JWT = z.infer<typeof jwtSchema>
 export const AuthContext = createContext<{
-  auth: string,
-  setAuth: React.Dispatch<React.SetStateAction<string>>
+  auth: JWT,
+  setAuth: React.Dispatch<React.SetStateAction<JWT>>
 }>(null!)
 export const useAuth = () => useContext(AuthContext)
 
+
 export function AuthProvider({ children }: React.PropsWithChildren) {
-  const [auth, setAuth] = useState<string>("")
+  const [auth, setAuth] = useState<JWT>({})
 
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
